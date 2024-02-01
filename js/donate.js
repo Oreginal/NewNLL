fda735140e08b343f74f63e5db7b6a574dca14ff
Oreutlwile Diutlwileng <oreutlwilediutlwileng@gmail.com>
2024-02-01 15:01:32 +0200

    about-us&causes

---
+++ js/donate.js
@@ -0,0 +1,9 @@
+function showPopup() {
+    event.preventDefault();
+
+    document.getElementById("popupModal").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popupModal").style.display = "none";
+}
